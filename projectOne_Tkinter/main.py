215ca08bef466859d8c4764f11707c2d252fbc3a
Kirati K <kiviniemikj@gmail.com>
2020-09-04 08:30:36 -0400

    Update main.py

--- projectOne_Tkinter/main.py
+++ projectOne_Tkinter/main.py
@@ -23,6 +23,7 @@ allies: int = 0
 
 # Variables to track shop costs
 strengthCost: int = 20
+alliesCost: int = 50
 
 
 
@@ -55,8 +56,23 @@ def buy_strength():
     global strengthCost
     if gold >= strengthCost:
         gold -= strengthCost
-        goldLabel.config(text=f"Gold: {gold}")
+        strengthCost = round(strengthCost * 1.5)
         strength += 1
+        goldLabel["text"] = f"Gold: {gold}"
+        upgradeStrengthCost["text"] = f"{strengthCost}"
+        statsStrength["text"] = f"Strength: {strength}"
+
+def buy_allies():
+    global gold
+    global allies
+    global alliesCost
+    if gold >= alliesCost:
+        gold -= alliesCost
+        alliesCost = round(alliesCost * 2)
+        allies += 1
+        goldLabel["text"] = f"Gold: {gold}"
+        upgradeAlliesCost["text"] = f"{alliesCost}"
+        statsAllies["text"] = f"Allies: {allies}"
 
 
 # Function to kill enemy
@@ -79,8 +95,9 @@ def death():
 def allies_attack():
     global enemyHealth
     global enemyHealthCurrent
+    global strength
 
-    enemyHealthCurrent -= allies
+    enemyHealthCurrent -= allies * strength
     healthBar["value"] = enemyHealthCurrent
 
     # If enemy dies
@@ -133,6 +150,15 @@ rightPanel.grid(sticky="ns", column=2, row=0)
 leftLabel = Label(leftPanel, text="Left Panel", bg="#1e1e1e", fg="#f1f1f1")
 leftLabel.grid()
 
+statsTitle = Label(leftPanel, text="Statistics", bg="#1e1e1e", fg="#f1f1f1", pady=50)
+statsTitle.grid(row=1)
+
+statsStrength = Label(leftPanel, text=f"Strength: {strength}", bg="#1e1e1e", fg="#f1f1f1")
+statsStrength.grid(row=2)
+
+statsAllies = Label(leftPanel, text=f"Allies: {allies}", bg="#1e1e1e", fg="#f1f1f1")
+statsAllies.grid(row=3)
+
 
 
 
@@ -170,8 +196,18 @@ goldLabel = Label(rightPanel, text=f"Gold: {gold}", bg="#1e1e1e", fg="#f1f1f1", 
 goldLabel.grid(row=1)
 
 # Strength upgrade button
-strengthButton = Button(rightPanel, text="Upgrade your strength!", height=2, width=20, command=buy_strength)
-strengthButton.grid(row=2)
+upgradeStrength = Button(rightPanel, text="Upgrade your strength!", height=2, width=20, command=buy_strength)
+upgradeStrength.grid(row=2)
+
+upgradeStrengthCost = Label(rightPanel, text=f"{strengthCost}", bg="#1e1e1e", fg="#f1f1f1", padx=10)
+upgradeStrengthCost.grid(row=2, column=1)
+
+# Allies upgrade button
+upgradeAllies = Button(rightPanel, text="Hire new allies!", height=2, width=20, command=buy_allies)
+upgradeAllies.grid(row=3)
+
+upgradeAlliesCost = Label(rightPanel, text=f"{alliesCost}", bg="#1e1e1e", fg="#f1f1f1", padx=10)
+upgradeAlliesCost.grid(row=3, column=1)
 
 
 
