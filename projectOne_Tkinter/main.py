b061523e50e074018e9c7ac6adcf55d67dc08b4b
Kirati K <kiviniemikj@gmail.com>
2020-09-06 10:08:40 -0400

    main.py Overhaul Continued

--- projectOne_Tkinter/main.py
+++ projectOne_Tkinter/main.py
@@ -27,11 +27,11 @@ from random import randint
 # multiple sticky notes.
 
 
-class EnemyValues():
+class EnemyValues(dict):
     """This class tracks all values and methods related to the ENEMY."""
 
     def __init__(self):
-        """Initialize the ENEMY from scratch."""
+        """Initialize the ENEMY from scratch with starting values."""
         self.reset()
 
     def reset(self):
@@ -42,43 +42,71 @@ class EnemyValues():
     def death(self):
         """Reset the values of ENEMY when killed."""
         self.randomize_name()
-        self.health = round(randint(25, 50) * PLAYER.kill_count / 10)
-        self.health_current = self.health
+
+        # This code is for the health scaling of the ENEMY. Basically, the math
+        # if a random number between 25 and 50 that gets multiplied by the
+        # number of kills the player has divided by 10. To have scaling health
+        # just the kill count could have been enough, but it would multiply
+        # too quickly and feel terrible for the player, therefore we dilute
+        # it by dividing the value by 10,
+        self["health"] = round(randint(25, 50) * PLAYER['kill_count'] / 10)
+        self["health_current"] = self["health"]
 
     def randomize_name(self):
         """Randomize the name value."""
-        self.name = (f"{firstNames[randint(0, len(firstNames) - 1)]}"
-                     + f" {lastNames[randint(0, len(lastNames) - 1)]}")
+        # Since I wanted it to be easy to input new names, I have the randint
+        # look through the first value of the list to the last. Because the
+        # last can now scale, we don't have to edit this code later. The len
+        # function counts the total items in a list using human counting,
+        # starting at 1. Because computers start with 0, we have to subtract
+        # 1 to make sure the values stay valid.
+        self["name"] = (f"{firstNames[randint(0, len(firstNames) - 1)]}"
+                        + f" {lastNames[randint(0, len(lastNames) - 1)]}")
 
     def randomize_health(self):
         """Randomize the health value."""
-        self.health = round(randint(25, 50) * 1 / 10)
-        self.health_current = self.health
-    name: str
-    health: int
-    health_current: int
+        # Basically the same code above but used in initialization to make sure
+        # the program doesn't crash by division by zero.
+        self["health"] = round(randint(25, 50) * 1 / 10)
+        self["health_current"] = self["health"]
 
 
-class PlayerValues:
+class PlayerValues(dict):
     """This class tracks all values and methods realted to the PLAYER."""
 
     def __init__(self):
-        """Initialize the PLAYER with a random name."""
-        self.name = (f"{firstNames[randint(0, len(firstNames) - 1)]}"
-                     + f" {lastNames[randint(0, len(lastNames) - 1)]}")
-    name: str
-    strength: int = 1
-    crit_chance: int = 5
-    allies: int = 0
-    gold: int = 0
-    kill_count: int = 0
+        """Initialize the PLAYER with a random name and starting values."""
+        # Same name code for the PLAYER as the ENEMY.
+        self["name"] = (f"{firstNames[randint(0, len(firstNames) - 1)]}"
+                        + f" {lastNames[randint(0, len(lastNames) - 1)]}")
+
+        self["strength"]: int = 1
+        self["crit_chance"]: int = 5
+        self["allies"]: int = 0
+        self["gold"]: int = 0
+        self["kill_count"]: int = 0
+
+    def upgrade(self, value, quantity, cost):
+        """Use for easily creating upgrades."""
+        if self['gold'] < cost:
+            print_console("Insufficient gold")
+            return False
+        self[value] += quantity
+        self['gold'] -= cost
+        return True
 
 
-class ShopValues:
+class ShopValues(dict):
     """This class tracks all values and methods related to the SHOP."""
 
-    strength_cost: int = 20
-    allies_cost: int = 50
+    def __init__(self):
+        """Initialize starting values for SHOP."""
+        self["strength_cost"]: int = 10
+        self["allies_cost"]: int = 25
+
+    def cost_multiplier(self, cost, multiplier):
+        """Multiply cost by multiplier."""
+        self[cost] = round(self[cost] * multiplier)
 
 
 #       VARIABLES      #
@@ -149,69 +177,65 @@ def print_console(value):
 def critical_hit():
     """Return true or false based on random chance."""
     crit: int = randint(1, 100)
-    return bool(crit <= PLAYER.crit_chance)
+    return bool(crit <= PLAYER['crit_chance'])
 
 
 def buy_strength():
     """Increase strength value of PLAYER."""
-    if PLAYER.gold >= SHOP.strength_cost:
-        PLAYER.gold -= SHOP.strength_cost
-        SHOP.strength_cost = round(SHOP.strength_cost * 1.5)
-        PLAYER.strength += 1
-        goldLabel["text"] = f"Gold: {PLAYER.gold}"
-        upgradeStrengthCost["text"] = f"{SHOP.strength_cost}"
-        statsStrength["text"] = f"Strength: {PLAYER.strength}"
+    if PLAYER.upgrade('strength', 1, SHOP['strength_cost']):
+        SHOP['strength_cost'] = round(SHOP['strength_cost'] * 1.25)
+
+        goldLabel["text"] = f"Gold: {PLAYER['gold']}"
+        upgradeStrengthCost["text"] = f"{SHOP['strength_cost']}"
+        statsStrength["text"] = f"Strength: {PLAYER['strength']}"
 
         print_console("Strength upgraded.\n")
-
-    else:
-        print_console("Insufficient Gold.\n")
 
 
 def buy_allies():
     """Increase ally value of PLAYER."""
-    if PLAYER.gold >= SHOP.allies_cost:
-        PLAYER.gold -= SHOP.allies_cost
-        SHOP.allies_cost = round(SHOP.allies_cost * 2)
-        PLAYER.allies += 1
-        goldLabel["text"] = f"Gold: {PLAYER.gold}"
-        upgradeAlliesCost["text"] = f"{SHOP.allies_cost}"
-        statsAllies["text"] = f"Allies: {PLAYER.allies}"
+    if PLAYER.upgrade('allies', 1, SHOP['allies_cost']):
+        SHOP.cost_multiplier('allies_cost', 1.5)
+
+        goldLabel["text"] = f"Gold: {PLAYER['gold']}"
+        upgradeAlliesCost["text"] = f"{SHOP['allies_cost']}"
+        statsAllies["text"] = f"Allies: {PLAYER['allies']}"
 
         print_console("Ally hired.\n")
-
-    else:
-        print_console("Insufficient Gold.\n")
 
 
 def kill():
     """Execute when the enemy is killed."""
+    # Since this is an RPG, randomness is commmon. To add some more, I wanted
+    # the gold gain to be random too. This code also lets the gold scale with
+    # ENEMY health so that it doesn't have to be tweaked. The math is simply
+    # ENEMY health divided by 5 multiplied by a random number between 1 and 3.
     random_multiplier = randint(1, 3)
-    gold_gain = round((ENEMY.health / 5) * random_multiplier)
-    PLAYER.gold += gold_gain
-    PLAYER.kill_count += 1
-    print_console(f"{ENEMY.name} Killed\nReceived {gold_gain} Gold!\n")
+    gold_gain = round((ENEMY['health'] / 5) * random_multiplier)
+    PLAYER['gold'] += gold_gain
+    PLAYER['kill_count'] += 1
+    print_console(f"{ENEMY['name']} Killed\nReceived {gold_gain} Gold!\n")
 
     ENEMY.death()
 
-    if ENEMY.name == PLAYER.name:
+    if ENEMY['name'] == PLAYER['name']:
         print_console("You found yourself!\n")
 
-    healthBar.config(maximum=ENEMY.health, value=ENEMY.health)
-    goldLabel["text"] = f"Gold: {PLAYER.gold}"
+    healthBar.config(maximum=ENEMY['health'], value=ENEMY['health'])
+    goldLabel["text"] = f"Gold: {PLAYER['gold']}"
 
-    statsKillCount["text"] = f"Kills: {PLAYER.kill_count}"
+    statsKillCount["text"] = f"Kills: {PLAYER['kill_count']}"
 
 
 def allies_attack():
     """Ally damage loop."""
-    ENEMY.health_current -= PLAYER.allies * PLAYER.strength
-    healthBar["value"] = ENEMY.health_current
+    ENEMY['health_current'] -= PLAYER['allies'] * PLAYER['strength']
+    healthBar["value"] = ENEMY['health_current']
 
     if healthBar["value"] <= 0:
         kill()
 
-    healthNumber.config(text=f"{ENEMY.health_current}/{ENEMY.health}")
+    healthNumber.config(text=f"{ENEMY['health_current']}/{ENEMY['health']}")
     # Continue the loop
     window.after(1000, allies_attack)
 
@@ -220,17 +244,17 @@ def allies_attack():
 def attack():
     """Player damage per click."""
     if critical_hit():
-        ENEMY.health_current -= PLAYER.strength * 2
-        print_console(f"Critical hit on {ENEMY.name}!\n")
+        ENEMY['health_current'] -= PLAYER['strength'] * 2
+        print_console(f"Critical hit on {ENEMY['name']}!\n")
     else:
-        ENEMY.health_current -= PLAYER.strength
+        ENEMY['health_current'] -= PLAYER['strength']
 
-    healthBar["value"] = ENEMY.health_current
+    healthBar["value"] = ENEMY['health_current']
 
-    if ENEMY.health_current <= 0:
+    if ENEMY['health_current'] <= 0:
         kill()
 
-    healthNumber.config(text=f"{ENEMY.health_current}/{ENEMY.health}")
+    healthNumber.config(text=f"{ENEMY['health_current']}/{ENEMY['health']}")
 
 
 #    PANEL CREATION    #
@@ -290,7 +314,7 @@ statsTitle.grid(pady=(50, 20))
 
 # PLAYER's randomized name
 statsName = tkinter.Label(leftPanel,
-                          text=PLAYER.name,
+                          text=PLAYER['name'],
                           bg="#1e1e1e", fg="#f1f1f1",
                           font="System")
 
@@ -298,7 +322,7 @@ statsName.grid(row=1, pady=(0, 30))
 
 # PLAYERS current strength
 statsStrength = tkinter.Label(leftPanel,
-                              text=f"Strength: {PLAYER.strength}",
+                              text=f"Strength: {PLAYER['strength']}",
                               bg="#1e1e1e", fg="#f1f1f1",
                               height=2, width=20,
                               font=("System", 12))
@@ -307,7 +331,7 @@ statsStrength.grid(row=2)
 
 # PLAYERS current allies
 statsAllies = tkinter.Label(leftPanel,
-                            text=f"Allies: {PLAYER.allies}",
+                            text=f"Allies: {PLAYER['allies']}",
                             bg="#1e1e1e", fg="#f1f1f1",
                             height=2, width=20,
                             font=("System", 12))
@@ -316,7 +340,7 @@ statsAllies.grid(row=3)
 
 # PLAYERS current kill count
 statsKillCount = tkinter.Label(leftPanel,
-                               text=f"Kills: {PLAYER.kill_count}",
+                               text=f"Kills: {PLAYER['kill_count']}",
                                bg="#1e1e1e", fg="#f1f1f1",
                                height=2, width=20,
                                font=("System", 12))
@@ -361,14 +385,15 @@ healthFrame.grid()
 healthBar = ttk.Progressbar(healthFrame,
                             mode='determinate',
                             style="red.Horizontal.TProgressbar",
-                            maximum=ENEMY.health,
-                            value=ENEMY.health, length=200)
+                            maximum=ENEMY['health'],
+                            value=ENEMY['health'], length=200)
 
 healthBar.grid(row=0)
 
 # ENEMY precise health value
 healthNumber = tkinter.Label(healthFrame,
-                             text=f"{ENEMY.health_current}/{ENEMY.health}",
+                             text=f"{ENEMY['health_current']}"
+                                  + f"/{ENEMY['health']}",
                              bg="#1e1e1e", fg="#f1f1f1",
                              width=5,
                              padx=10, pady=50,
@@ -384,7 +409,7 @@ enemy.grid(row=1)
 
 # ENEMY randomized name
 enemyName = tkinter.Label(middlePanel,
-                          text=ENEMY.name,
+                          text=ENEMY['name'],
                           bg="#1e1e1e", fg="#f1f1f1",
                           font="System")
 enemyName.grid(row=2, pady=(0, 30))
@@ -399,7 +424,7 @@ enemyName.grid(row=2, pady=(0, 30))
 
 # PLAYER gold count
 goldLabel = tkinter.Label(rightPanel,
-                          text=f"Gold: {PLAYER.gold}",
+                          text=f"Gold: {PLAYER['gold']}",
                           bg="#1e1e1e", fg="#f1f1f1",
                           pady=50,
                           font=("System", 20))
@@ -416,7 +441,7 @@ upgradeStrength.grid(row=2)
 
 # SHOP buy strength cost
 upgradeStrengthCost = tkinter.Label(rightPanel,
-                                    text=SHOP.strength_cost,
+                                    text=SHOP['strength_cost'],
                                     bg="#1e1e1e", fg="#f1f1f1",
                                     padx=10,
                                     font=("System", 12))
@@ -433,7 +458,7 @@ upgradeAllies.grid(row=3)
 
 # SHOP buy allies cost
 upgradeAlliesCost = tkinter.Label(rightPanel,
-                                  text=SHOP.allies_cost,
+                                  text=SHOP['allies_cost'],
                                   bg="#1e1e1e", fg="#f1f1f1",
                                   padx=10,
                                   font=("System", 12))
