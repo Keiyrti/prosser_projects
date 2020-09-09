7724f241545b2c2366629509acdb6df9588e8593
Kirati K <kiviniemikj@gmail.com>
2020-09-08 22:58:04 -0400

    game.py Update

--- projectOne_Tkinter/game.py
+++ projectOne_Tkinter/game.py
@@ -19,6 +19,7 @@ from random import randint
 
 class PlayerValues(dict):
     """Define values and modules for the PLAYER object."""
+
     def __init__(self):
         """Initialize PLAYER with a random name and starting values."""
         self.random_name()
@@ -48,7 +49,7 @@ class PlayerValues(dict):
 
     def critical_hit(self):
         """Return value for critical hits."""
-        _crit = randint(1,100)
+        _crit = randint(1, 100)
         if _crit <= self["crit_chance"]:
             return True
         return False
@@ -79,6 +80,7 @@ class PlayerValues(dict):
 
 class EnemyValues(dict):
     """Define values and modules for the ENEMY object."""
+
     def __init__(self):
         """Initialize ENEMY with a random name."""
         self.random_name()
@@ -91,7 +93,7 @@ class EnemyValues(dict):
 
     def random_health(self):
         """Give ENEMY a random health value."""
-        self["health_max"] = round(randint(40,50) * PLAYER["stage"] / 10)
+        self["health_max"] = round(randint(40, 50) * PLAYER["stage"] / 10)
         self["health"] = self["health_max"]
 
 
@@ -113,12 +115,14 @@ def print_console(value):
     console_window.insert("end", f"{value}\n")
     console_window.see("end")
 
+
 def crit_message(name):
     """Switch critical hit message depending on name."""
     switcher = {
         'Jotaro Joestar': "Ora ora ora!"
     }
     return switcher.get(name, "Critical hit!")
+
 
 def death():
     """Activate when enemy dies to reset values."""
@@ -133,6 +137,7 @@ def death():
                                        + f"/{ENEMY['health_max']}")
         PLAYER['stage'] += 1
 
+
 def attack(event):
     """Activate when PLAYER clicks to damage ENEMY."""
     ENEMY['health'] -= PLAYER.attack()
@@ -140,6 +145,7 @@ def attack(event):
     enemy_health_bar['value'] = ENEMY['health']
     enemy_health_number['text'] = (f"{ENEMY['health']}"
                                    + f"/{ENEMY['health_max']}")
+
 
 def game_tick():
     """Store all loops."""
@@ -157,15 +163,15 @@ def game_tick():
 root = tkinter.Tk()
 root.title("Endless Expansion V1.0")
 
-root.grid_columnconfigure([0,2],
-                     weight=225,
-                     minsize=225)
+root.grid_columnconfigure([0, 2],
+                          weight=225,
+                          minsize=225)
 root.grid_columnconfigure(1,
-                     weight=350,
-                     minsize=350)
+                          weight=350,
+                          minsize=350)
 root.grid_rowconfigure(0,
-                  weight=600,
-                  minsize=600)
+                       weight=600,
+                       minsize=600)
 
 
 # CREATE PANELS #
@@ -177,12 +183,12 @@ left_panel.grid(row=0, column=0,
                 sticky="nsew")
 
 middle_panel = tkinter.Frame(root,
-                           bg="#1e1e1e")
+                             bg="#1e1e1e")
 middle_panel.grid(row=0, column=1,
-                sticky="nsew")
+                  sticky="nsew")
 
 right_panel = tkinter.Frame(root,
-                           bg="#1e1e1e")
+                            bg="#1e1e1e")
 right_panel.grid(row=0, column=2,
                  sticky="nsew")
 
@@ -191,39 +197,42 @@ right_panel.grid(row=0, column=2,
 
 
 left_panel.grid_columnconfigure(0,
-                           weight=225,
-                           minsize=225)
+                                weight=225,
+                                minsize=225)
 left_panel.grid_rowconfigure(0,
-                        weight=75,
-                        minsize=75)
+                             weight=75,
+                             minsize=75)
 left_panel.grid_rowconfigure(1,
-                        weight=425,
-                        minsize=425)
+                             weight=425,
+                             minsize=425)
 left_panel.grid_rowconfigure(2,
-                        weight=100,
-                        minsize=100)
+                             weight=100,
+                             minsize=100)
 
 player_name = tkinter.Label(left_panel,
                             bg="#1e1e1e",
                             fg="#f1f1f1",
-                            text = PLAYER["name"],
-                            font = ("System", 18))
-player_name.grid(row=0,column=0,
+                            text=PLAYER["name"],
+                            font=("System", 18))
+player_name.grid(row=0, column=0,
                  sticky="nsew")
 
 
 player_stats = tkinter.Frame(left_panel,
                              bg="#2e2e2e")
-player_stats.grid(row=1,column=0,
+player_stats.grid(row=1, column=0,
                   padx=20, pady=10,
                   sticky="nsew")
 
+#player_stats.grid_columnconfigure(0,
+                                #)
+
 player_skills = tkinter.Frame(left_panel,
-                             bg="#1e1e1e")
+                              bg="#1e1e1e")
 player_skills.grid(row=2, column=0,
                    sticky="nsew", padx=15)
 
-player_skills.grid_columnconfigure([0,1,2],
+player_skills.grid_columnconfigure([0, 1, 2],
                                    weight=75,
                                    minsize=25)
 player_skill_one_bar = ttk.Progressbar(player_skills,
@@ -235,7 +244,7 @@ player_skill_one_bar.grid(row=0, column=0,
                           padx=5, pady=5, sticky='ew')
 
 player_skill_one = tkinter.Button(player_skills,
-                                  width=5, height = 2,
+                                  width=5, height=2,
                                   bg="#2e1e1e", fg="#f1f1f1",
                                   text="Athena",
                                   font="System")
@@ -243,7 +252,7 @@ player_skill_one.grid(row=1, column=0,
                       padx=5, pady=5, sticky='ew')
 
 player_skill_two = tkinter.Button(player_skills,
-                                  width=5, height = 2,
+                                  width=5, height=2,
                                   bg="#2e1e1e", fg="#f1f1f1",
                                   text="Midas",
                                   font="System")
@@ -251,7 +260,7 @@ player_skill_two.grid(row=1, column=1,
                       padx=5, pady=5, sticky='ew')
 
 player_skill_three = tkinter.Button(player_skills,
-                                    width=5, height = 2,
+                                    width=5, height=2,
                                     bg="#2e1e1e", fg="#f1f1f1",
                                     text="Ares",
                                     font="System")
@@ -268,32 +277,32 @@ player_skill_two_bar.grid(row=0, column=1,
                           padx=5, pady=5, sticky='ew')
 
 player_skill_three_bar = ttk.Progressbar(player_skills,
-                                       mode="determinate",
-                                       max=60,
-                                       value=30,
-                                       length=40)
+                                         mode="determinate",
+                                         max=60,
+                                         value=30,
+                                         length=40)
 player_skill_three_bar.grid(row=0, column=2,
-                          padx=5, pady=5, sticky='ew')
+                            padx=5, pady=5, sticky='ew')
 
 
 # MIDDLE PANEL ASSETS #
 
 
 middle_panel.grid_columnconfigure(0,
-                           weight=225,
-                           minsize=225)
+                                  weight=225,
+                                  minsize=225)
 middle_panel.grid_rowconfigure(0,
-                        weight=75,
-                        minsize=75)
+                               weight=75,
+                               minsize=75)
 middle_panel.grid_rowconfigure(1,
-                        weight=300,
-                        minsize=300)
+                               weight=300,
+                               minsize=300)
 middle_panel.grid_rowconfigure(2,
-                        weight=225,
-                        minsize=225)
+                               weight=225,
+                               minsize=225)
 enemy_health = tkinter.Frame(middle_panel,
-                     bg="#1e1e1e")
-enemy_health.grid(row=0,column=0)
+                             bg="#1e1e1e")
+enemy_health.grid(row=0, column=0)
 
 
 enemy_health_bar = ttk.Progressbar(enemy_health,
@@ -301,7 +310,7 @@ enemy_health_bar = ttk.Progressbar(enemy_health,
                                    max=ENEMY["health_max"],
                                    value=ENEMY["health"],
                                    length=300)
-enemy_health_bar.grid(row=0,column=0)
+enemy_health_bar.grid(row=0, column=0)
 
 
 enemy_health_number = tkinter.Label(enemy_health,
@@ -314,7 +323,7 @@ enemy_health_number.grid(row=1, column=0)
 enemy_health.lower(belowThis=enemy_health_bar)
 
 enemy = tkinter.Frame(middle_panel,
-              bg="#1e1e1e")
+                      bg="#1e1e1e")
 
 enemy.grid(row=1, column=0)
 
@@ -345,7 +354,7 @@ console_window = tkinter.Text(console,
                               bg="#1e1e1e", fg="#f1f1f1",
                               width=40, height=12,
                               font="System")
-console_window.grid(row=0, column=0, sticky='nsew', ipadx=5)
+console_window.grid(row=0, column=0, sticky='nsew')
 
 console_scrollbar = tkinter.Scrollbar(console)
 console_scrollbar.grid(row=0, column=1, sticky="ns")
@@ -358,47 +367,47 @@ console_scrollbar.config(command=console_window.yview)
 
 
 right_panel.grid_columnconfigure(0,
-                           weight=225,
-                           minsize=225)
+                                 weight=225,
+                                 minsize=225)
 right_panel.grid_rowconfigure(0,
-                        weight=75,
-                        minsize=75)
+                              weight=75,
+                              minsize=75)
 right_panel.grid_rowconfigure(1,
-                        weight=116,
-                        minsize=116)
+                              weight=116,
+                              minsize=116)
 right_panel.grid_rowconfigure(2,
-                        weight=232,
-                        minsize=232)
+                              weight=232,
+                              minsize=232)
 right_panel.grid_rowconfigure(3,
-                        weight=175,
-                        minsize=175)
+                              weight=175,
+                              minsize=175)
 
 
 shop_name = tkinter.Label(right_panel,
-                            bg="#1e1e1e",
-                            fg="#f1f1f1",
-                            text = "Shop",
-                            font = ("System", 18))
-shop_name.grid(row=0,column=0,
-                 sticky="nsew")
-
-shop_stats = tkinter.Frame(right_panel,
-                             bg="#2e2e2e")
-shop_stats.grid(row=1,column=0,
-                  padx=20, pady=10,
-                  sticky="nsew")
-
-shop_allies = tkinter.Frame(right_panel,
-                          bg="#2e2e2e")
-shop_allies.grid(row=2,column=0,
-               padx=20, pady=10,
+                          bg="#1e1e1e",
+                          fg="#f1f1f1",
+                          text="Shop",
+                          font=("System", 18))
+shop_name.grid(row=0, column=0,
                sticky="nsew")
 
-shop_skills = tkinter.Frame(right_panel,
+shop_stats = tkinter.Frame(right_panel,
                            bg="#2e2e2e")
-shop_skills.grid(row=3,column=0,
-                padx=20, pady=(10,20),
+shop_stats.grid(row=1, column=0,
+                padx=20, pady=10,
                 sticky="nsew")
+
+shop_allies = tkinter.Frame(right_panel,
+                            bg="#2e2e2e")
+shop_allies.grid(row=2, column=0,
+                 padx=20, pady=10,
+                 sticky="nsew")
+
+shop_skills = tkinter.Frame(right_panel,
+                            bg="#2e2e2e")
+shop_skills.grid(row=3, column=0,
+                 padx=20, pady=(10, 20),
+                 sticky="nsew")
 
 
 # INITIATE ROOT #
