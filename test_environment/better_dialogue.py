b502e16820de7472cd8861bd0bcc3d7a932c74f2
Keiyrti K <Keiyrti_K@users.noreply.github.com>
2020-09-17 23:49:42 -0400

    Update better_dialogue.py

--- test_environment/better_dialogue.py
+++ test_environment/better_dialogue.py
@@ -63,7 +63,8 @@ class Dialogue:
 
         self.locked = False
 
-        self.text_box.bind('<Button-1>', self.skip)
+        self.text_box.bind('<Button-1>', self.click_event)
+        self.master.bind('<Return>', self.hyper_action)
 
     def __repr__(self):
         return "Dialogue Box Object"
@@ -76,12 +77,14 @@ class Dialogue:
                 self.items_next = 1
                 self.items_prev = -1
                 self.char_index = 0
+                self.char_max = 0
                 self.items_max = len(value) - 1
                 self.speed = 50
                 self.items_completed = False
                 self.bind = False
-                self.skip_speed = False
+                self.skip = False
                 self.printing = None
+                self.hyper = False
                 self.action()
             else:
                 self.items = []
@@ -98,52 +101,107 @@ class Dialogue:
 
         self.items_completed = False
 
+        if self.hyper == True:
+            self.hyper_action()
+        else:
+            self.action()
+
+    def click_event(self, event=None):
+        if self.items_completed == True:
+            pass
+        else:
+            self.skip = True
+            if self.printing != None:
+                self.master.after_cancel(self.printing)
+                self.printing = None
         self.action()
 
-    def skip(self, event):
-        if self.items_completed == True:
-            self.action()
-        else:
-            print("skip speed!")
-            self.skip_speed = True
-
     def action(self, event=None):
-        _item = self.items[self.items_index]
+        print(f"Current Item: {self.items_index}")
+        print(f"Max Items: {self.items_max}")
         if self.items == [] or self.locked == True or self.items_index > self.items_max:
             pass
         elif self.items_completed == True:
+            self.skip = False
             self.update()
-        elif isinstance(self.items[self.items_index], str):
-            self.char_max = len(self.items[self.items_index])
-            if self.bind == False:
-                self.text = ''
-            else:
+        elif self.skip == True:
+            _item = self.items[self.items_index]
+
+            if isinstance(_item, str):
+                self.char_max = len(_item)
                 self.bind = False
-            self.print()
-        elif isinstance(_item, float):
-            _pause = round(_item)
-            self.printing = self.master.after(_pause, self.update)
-        elif isinstance(self.items[self.items_index], int):
-            self.speed = self.items[self.items_index]
-            self.update()
+                self.print()
+            elif isinstance(_item, float):
+                self.update()
+            elif isinstance(_item, int):
+                self.speed = _item
+                self.update()
+        else:
+            _item = self.items[self.items_index]
+
+            if isinstance(_item, str):
+                self.char_max = len(_item)
+                if self.bind == False:
+                    self.text = ''
+                else:
+                    self.bind = False
+                self.print()
+            elif isinstance(_item, float):
+                _pause = round(_item)
+                if self.skip == True:
+                    self.update()
+                else:
+                    self.printing = self.master.after(_pause, self.update)
+            elif isinstance(_item, int):
+                self.speed = _item
+                self.update()
+
+    def hyper_action(self, event=None):
+        self.hyper = True
+        self.skip = True
+        if self.printing != None:
+            self.master.after_cancel(self.printing)
+            self.printing = None
+        for _items in self.items:
+            if self.items == [] or self.locked == True or self.items_index > self.items_max:
+                self.hyper = False
+                self.skip = False
+            elif self.items_completed == True:
+                self.update()
+            elif self.hyper == True:
+                _item = self.items[self.items_index]
+                if isinstance(_item, str):
+                    self.char_max = len(_item)
+                    if self.bind == True:
+                        self.bind = False
+                    else:
+                        self.text = ''
+                    self.print()
+                elif isinstance(_item, float):
+                    self.update()
+                elif isinstance(_item, int):
+                    self.speed = _item
+                    self.update()
 
     def print(self):
         _speed = self.speed
-        if self.skip_speed == True:
-            _speed = 0
 
-
-        if self.char_index == self.char_max:
+        print(f"Max Char: {self.char_max}")
+        print(f"Current Char: {self.char_index}")
+        if self.char_index >= self.char_max:
             self.printing = None
             self.items_completed = True
         elif self.items[self.items_index][self.char_index] == '+':
-            self.items_completed = True
+            print('PLUS!')
             self.bind = True
             self.update()
         else:
             self.text_box['text'] += self.items[self.items_index][self.char_index]
             self.char_index += 1
-            self.printing = self.master.after(_speed, self.print)
+            if self.skip == True:
+                self.print()
+            else:
+                self.printing = self.master.after(_speed, self.print)
 
 
 
@@ -162,10 +220,14 @@ if __name__ == '__main__':
     # Import Dialogue
     _dialogue = Dialogue(root)
 
-    _items = ["So+", 300, "...+", 1000.0, 50, " Is this how it works?", "It all works!"]
+    _items = ["So+", 300, "...+", 1000.0, 50, " Is this how it works?",
+              100,"I just want to make sure...+", 300.0, " Ya know?",
+              "Here",
+              "Is",
+              "Some",
+              "Random",
+              "Testing"]
 
     _dialogue.items = _items
-
-    print(_dialogue.items_index)
 
     root.mainloop()
